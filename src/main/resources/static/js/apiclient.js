b434920a5e80e0c02c641ef6778cb26ec5e985a4
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2020-10-22 14:59:40 -0500

    Commit 6

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -1,8 +1,9 @@
+var Url = "http://"+$(location).attr('hostname')+":"+$(location).attr('port')+"/clima/";
 api = (function () {
     return {
         getClimaByCiudad: function (ciudad, callback) {
             var f;
-                $.get("/clima/"+ciudad,function(data){
+                $.get(Url+ciudad,function(data){
                     f=data;
                 });
                 return callback(f)
